728ca3cce221f5d0f7be56d229a8e027d50f9946
Alexander <neumann@dbis.rwth-aachen.de>
2018-03-21 12:16:53 +0100

    helper scripts

--- data_helpers.py
+++ data_helpers.py
@@ -1,8 +1,4 @@
-import numpy as np
 import re
-import itertools
-from collections import Counter
-
 
 def clean_str(string):
     """
@@ -25,41 +21,19 @@ def clean_str(string):
     return string.strip().lower()
 
 
-def load_data_and_labels(positive_data_file, negative_data_file):
-    """
-    Loads MR polarity data from files, splits the data into words and generates labels.
-    Returns split sentences and labels.
-    """
-    # Load data from files
-    positive_examples = list(open(positive_data_file, "r").readlines())
-    positive_examples = [s.strip() for s in positive_examples]
-    negative_examples = list(open(negative_data_file, "r").readlines())
-    negative_examples = [s.strip() for s in negative_examples]
-    # Split by words
-    x_text = positive_examples + negative_examples
-    x_text = [clean_str(sent) for sent in x_text]
-    # Generate labels
-    positive_labels = [[0, 1] for _ in positive_examples]
-    negative_labels = [[1, 0] for _ in negative_examples]
-    y = np.concatenate([positive_labels, negative_labels], 0)
-    return [x_text, y]
+
+dataset = list(open("data/html.csv", "r").readlines())
+dataset = [s.strip() for s in dataset[1:]]
+dataset = [clean_str(sent) for sent in dataset]
+x_text = [s.split(" ") for s in dataset]
+dataset = [s[0:] for s in x_text]
+dataset_clean = []
+for s in dataset:
+    dataset_clean.append( [' '.join(ww for ww in s)])
+ 
 
 
-def batch_iter(data, batch_size, num_epochs, shuffle=True):
-    """
-    Generates a batch iterator for a dataset.
-    """
-    data = np.array(data)
-    data_size = len(data)
-    num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
-    for epoch in range(num_epochs):
-        # Shuffle the data at each epoch
-        if shuffle:
-            shuffle_indices = np.random.permutation(np.arange(data_size))
-            shuffled_data = data[shuffle_indices]
-        else:
-            shuffled_data = data
-        for batch_num in range(num_batches_per_epoch):
-            start_index = batch_num * batch_size
-            end_index = min((batch_num + 1) * batch_size, data_size)
-            yield shuffled_data[start_index:end_index]
+        
+for s in dataset_clean:
+    for w in s:
+        f = open('corpus/qa_data.html','a').write(w + '\n')
